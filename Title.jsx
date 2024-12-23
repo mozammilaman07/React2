136c3bee005bb05415ae43ce3ae5bc63f92a6c04
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-12-23 10:44:37 +0530

    wraped in a div

--- Title.jsx
+++ Title.jsx
@@ -1,11 +1,11 @@
 function Title({ title, desc, price }) {
   return (
-    <>
+    <div className="Title">
       <h1>{title}</h1>
       <h3>{desc}</h3>
       <p>price : {price}</p>
       {price >= 15000 && <p>Discount : 5%</p>}
-    </>
+    </div>
   );
 }
 
