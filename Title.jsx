3d733d9dba9ad0113f15f5c9b3dc8587994bd553
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-12-23 11:06:54 +0530

    added conditional css

--- Title.jsx
+++ Title.jsx
@@ -1,8 +1,9 @@
 import "./Title.css";
 
 function Title({ title, desc, price }) {
+  let styles = { backgroundColor: price > 15000 ? "pink" : "" };
   return (
-    <div className="title">
+    <div className="title" style={styles}>
       <h1>{title}</h1>
       <h3>{desc}</h3>
       <p>price : {price}</p>
