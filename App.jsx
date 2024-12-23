2839f0f76c7e3ff1911e585f24f96cc58796b947
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-12-23 10:42:13 +0530

    passed props

--- App.jsx
+++ App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+// import "./App.css";
+import Title from "./Title";
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <>
+      <Title title="Phone" desc="this i an iphone" price={14000} />;
+      <Title title="laptop" desc="this i a mac" price={40000} />;
+      <Title title="cloths" desc="this i an levis" price={1400} />;
+    </>
+  );
 }
 
 export default App;
