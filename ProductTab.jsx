6aa077f4042c02c2186b47daef39a76498613ef2
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-12-24 21:13:00 +0530

    passed index of array

--- ProductTab.jsx
+++ ProductTab.jsx
@@ -1,5 +1,12 @@
 import Product from "./Product";
 
 export default function ProductTab() {
-  return <Product />;
+  return (
+    <>
+      <Product title="Logitech" idx={0} />
+      <Product title="Apple Pen2" idx={1} />
+      <Product title="Petronics" idx={2} />
+      <Product title="Logitech" idx={3} />
+    </>
+  );
 }
