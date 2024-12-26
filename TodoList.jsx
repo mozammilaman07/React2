f5350b8a477369b2c073cccf52e8c91ba50dfab3
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-12-26 23:54:40 +0530

    changed position of h4

--- TodoList.jsx
+++ TodoList.jsx
@@ -1,9 +1,9 @@
 export default function TodoList() {
   return (
     <div>
-      <h4>Todo List</h4>
       <input type="text" />
       <button>Add</button>
+      <h4>Todo List</h4>
     </div>
   );
 }
