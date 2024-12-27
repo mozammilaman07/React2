25adb94feb184f15be2c9e194aaae133e50b87cc
Md Mozammil Aman <amanmozammil.07@gmail.com>
2024-12-27 22:33:02 +0530

    used useState

--- TodoList.jsx
+++ TodoList.jsx
@@ -1,7 +1,14 @@
 export default function TodoList() {
+  let [todos, setTodos] = useState(["sample Task"]);
+  let [newTodo, setNewTodo] = useState("");
+
+  let addNewTask = () => {
+    console.log("we have to add new task in todo");
+  };
+
   return (
     <div>
-      <input type="text" />
+      <input type="text" placeholder="add a Task" />
       <button>Add</button>
       <h4>Todo List</h4>
     </div>
