28cefbae2575bb6cd1d7a837335bc91fec903ad5
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-01-23 00:54:45 +0530

    import weather component

--- App.jsx
+++ App.jsx
@@ -3,12 +3,12 @@ import ProductTab from "./ProductTab";
 import Title from "./Title";
 import TodoList from "./TodoList";
 import LikeButton from "./LikeButton";
+import Weather from "./Weather";
 
 function App() {
   return (
     <>
-      <TodoList />
-      <LikeButton />
+      <Weather />
     </>
   );
 }
